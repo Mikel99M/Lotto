007bef1b690a05cb6cb723c5cbceb1dbe32051f4
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-10-25 13:56:39 +0200

    Added facades and packages

---
+++ src/test/java/com/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -0,0 +1,14 @@
+package com.lotto.domain.numberreceiver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberReceiverFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+
+}
---
+++ src/test/java/com/lotto/domain/resultchecker/ResultCheckerFacadeTest.java
@@ -0,0 +1,14 @@
+package com.lotto.domain.resultchecker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultCheckerFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+
+}
